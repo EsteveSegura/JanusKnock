bf370337b81eae48f1f8481aa5c8b0e3262d9c91
Esteve Segura <estevesegura.gir@gmail.com>
2024-11-24 20:38:40 +0100

    refactor: improve file creation for key in server mode

--- internal/common/config.go
+++ internal/common/config.go
@@ -21,8 +21,8 @@ func ParseFlags() *Config {
 	flag.StringVar(&config.Mode, "mode", "", "Mode of operation (server/client)")
 	flag.StringVar(&config.Host, "h", "localhost", "Target host (default: localhost)")
 	flag.StringVar(&config.Host, "host", "localhost", "Target host (default: localhost)")
-	flag.StringVar(&config.KeyFile, "f", "", "Path to the key file")
-	flag.StringVar(&config.KeyFile, "file", "", "Path to the key file")
+	flag.StringVar(&config.KeyFile, "f", "", "Path to the key file.\nIf file not exists we will create on the specified path (only server)")
+	flag.StringVar(&config.KeyFile, "file", "", "Path to the key file.\nIf file not exists we will create on the specified path (only server)")
 	flag.StringVar(&config.OnSuccess, "on-success", "", "Command to execute on successful port knocking (server mode)")
 
 	// Customize the Usage message
@@ -33,6 +33,7 @@ Options:
   -m, --mode          Mode of operation (server/client) [required]
   -h, --host          Target host (default: localhost)
   -f, --file          Path to the key file [optional for server, required for client]
+                      If file not exists we will create on the specified path (only server)
   --on-success        Command to execute on successful port knocking (server mode)
 `)
 	}
@@ -45,6 +46,20 @@ Options:
 		fmt.Fprintln(os.Stderr, "Error: -m/--mode is required")
 		flag.Usage()
 		os.Exit(1)
+	}
+
+	if config.KeyFile != "" && config.Mode == "server" {
+		if _, err := os.Stat(config.KeyFile); err != nil {
+			if os.IsNotExist(err) {
+				secret := GenerateSecret()
+
+				err := os.WriteFile(config.KeyFile, []byte(secret), 0600)
+				if err != nil {
+					fmt.Fprintf(os.Stderr, "Error creating key file: %v\n", err)
+					os.Exit(1)
+				}
+			}
+		}
 	}
 
 	// If --file is not specified and the mode is server, create a new key file
